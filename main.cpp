8b8e1ea26428a6fb843d8b9a8b9793eb47cdce5c
TimurNurp <93672090+TimurNurp@users.noreply.github.com>
2023-02-15 06:46:56 +0000

    modified:   main.cpp

--- main.cpp
+++ main.cpp
@@ -1,15 +1,17 @@
-void mergeSort(int* arr, int l, int r)
-{
-  if (l >= r)
-    return;
-  int m = (r-l-1)/2; //середина массива
-  
-  mergeSort(arr, l, m);
-  
-  mergeSort(arr, m+1, r);
-  
-  merge(arr, l, m, r); 
-}
+/*
+ - Доработайте алгоритм merge sort, описанный в 11 модуле, сделав его многопоточным.
+ - Используйте std::futurez в качестве инструмента для синхронизации результата работы потоков.
+ - Нужно определить независимые части алгоритма, которые могут быть распараллелены и запустить их вычисление в отдельных потоках, как это было сделано в юните 28.7.
+ - Потоки могут быть запущены через std::thread или std::async, но синхронизация строго через std::future. Также предусмотрите ограничение количества запускаемых 
+потоков по примеру из юнита 28.7.
+*/
+
+#include <future>
+#include <iostream>
+#include <chrono>
+#include <random>
+
+bool make_thread = true;
 
 void merge(int* arr, int l, int m, int r)
 {
@@ -52,4 +54,79 @@ void merge(int* arr, int l, int m, int r)
        j++;
        k++;
    }
+}
+
+void mergeSort(int* arr, int l, int r)
+{
+  if (l >= r)
+    return;
+  int m = (r-l-1)/2; //середина массива
+  
+
+
+  if(make_thread && (r - l > 10000))
+   {
+       // если элементов в левой части больше чем 10000
+       // вызываем асинхронно рекурсию для правой части
+       auto f = async(std::launch::async, [&](){
+             mergeSort(arr, l, m);
+       });
+       
+        mergeSort(arr, m+1, r);
+   } else {
+       // запускаем обе части синхронно
+        mergeSort(arr, l, m);
+        
+        mergeSort(arr, m+1, r);
+  }
+
+  merge(arr, l, m, r); 
+}
+
+int main()
+{
+   srand(0);
+   long arr_size = 100000000;
+   int* array = new int[arr_size];  //создали массив
+
+   for(long i=0; i<arr_size; i++) { //заполнили числами
+       array[i] = rand() % 500000;
+   }
+
+   time_t start, end;
+
+
+   // многопоточный запуск
+   time(&start);
+   mergeSort(array, 0, arr_size); //функция сортировки
+   time(&end);
+
+   double seconds = difftime(end, start);
+   printf("The time: %f seconds\n", seconds);
+
+   for(long i=0;i<arr_size-1; i++) { //проверка правильности сортировки
+       if (array[i] > array[i + 1]) {
+         std::cout << "Unsorted" << i << std::endl;
+         break;
+       }
+   }
+
+
+
+   for(long i=0;i<arr_size; i++) { //заполнили еще раз числами
+       array[i] = rand() % 500000;
+   }
+
+   // однопоточный запуск
+   make_thread = false;
+   time(&start);
+   mergeSort(array, 0, arr_size);
+   time(&end);
+
+   seconds = difftime(end, start);
+   printf("The time: %f seconds\n", seconds);
+   
+   delete [] array;
+   
+   return 0;
 }
